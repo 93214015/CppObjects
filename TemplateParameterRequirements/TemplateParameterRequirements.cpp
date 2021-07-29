4325fdecf47f5738fc8648dc353c2c10d1d5e810
93214015 <64466509+93214015@users.noreply.github.com>
2021-07-29 12:04:11 +0430

    Added TagDistapch, Constexpr If, Concepts

--- TemplateParameterRequirements/TemplateParameterRequirements.cpp
+++ TemplateParameterRequirements/TemplateParameterRequirements.cpp
@@ -2,6 +2,9 @@
 //
 
 #include <iostream>
+#include <concepts>
+
+
 
 
 
@@ -48,10 +51,96 @@ TemplatedFunctionSFINAE(T _arg0, T _arg1)
 
 
 
+
+
+/*
+* Tag Dispatch
+* 
+* this technique is used to call different version of a function
+*/
+
+template<class T>
+T FractionPart_Impl(T _Val, std::false_type)
+{
+    return 0;
+}
+
+template<class T>
+T FractionPart_Impl(T _Val, std::true_type)
+{
+    T _iPart;
+    return modf(_Val, &_iPart);
+}
+
+
+template<class T>
+struct NumberStructTag
+{
+    T _Number;
+
+    T FractionPart()
+    {
+        return FractionPart_Impl(_Number, std::is_floating_point<T>{});
+    }
+
+};
+
+
+
+
+/*
+* constexpr if
+*/
+
+template<class T>
+struct NumberStructConstexpr
+{
+    T _Number;
+
+    T FractionPart()
+    {
+        if constexpr (std::is_floating_point_v<T>)
+        {
+            T _iPart;
+            return modf(_Number, &_iPart);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+};
+
+
+
+/*
+* C++ 20 Concepts
+*/
+
+template<typename T>
+concept ConceptIsArithmetic = std::is_arithmetic_v<T>;
+
+template<ConceptIsArithmetic T>
+struct NumberStructConcept
+{
+    T _Number;
+
+    T FractionPart() requires std::is_floating_point_v<T>
+    {
+        T _iPart;
+        return modf(_Number, &_iPart);
+    }
+};
+
+
+
+
 int main()
 {
     TemplatedStructSFINAE<float> _Struct;// passed
     //TemplatedStructSFINAE<int> _Struct;// error
+
 
     auto _Result = TemplatedFunctionSFINAE(1, 2);// passed
     //auto _Result = TemplatedFunctionSFINAE(1.1, 2.2); //error
@@ -62,6 +151,23 @@ int main()
 
     _Number2.FractionPart(); //passes
     //_Number1.FractionPart(); error
+
+    NumberStructTag<double> _NumberTag{1.256};
+    NumberStructTag<int> _NumberTag2{ 2 };
+    
+    NumberStructConstexpr _NumberConstexpr1{ 2.56 }; // used C++ 17 Template parameter deduction
+    NumberStructConstexpr _NumberConstexpr2{ 2 };
+
+
+    NumberStructConcept _NumberConcept1{ 1 };
+    NumberStructConcept _NumberConcept2{ 2.23565 };
+    //NumberStructConcept<std::string> _NumberConcept3{ "ABC" }; // error - string is not arithmetic
+
+    //_NumberConcept1.FractionPart(); // error  - The int type doesn't have fraction() method
+    auto _Fraction = _NumberConcept2.FractionPart();
+
+    std::cout << _NumberConstexpr1.FractionPart() << "\n";
+    std::cout << _NumberConstexpr2.FractionPart() << "\n";
 
 
     std::cout << "Hello World!\n";
